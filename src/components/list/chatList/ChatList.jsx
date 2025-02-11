54e7c978f198a3f409ce138614207b751d311cf5
Artur <metallist15431543@gmail.com>
2025-02-11 20:44:47 +0300

    add style to ChatList component

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,7 +1,53 @@
+import { useState } from "react";
 import "./chatList.css";
 
 const ChatList = () => {
-  return <div className="chatList">ChatList</div>;
+  const [addMode, setAddMode] = useState(false);
+
+  return (
+    <div className="chatList">
+      <div className="search">
+        <div className="searchBar">
+          <img src="/search.png" alt="" />
+          <input type="text" placeholder="search" />
+        </div>
+        <img
+          src={addMode ? "./minus.png" : "./plus.png"}
+          alt=""
+          className="add"
+          onClick={() => setAddMode((prev) => !prev)}
+        />
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChatList;
